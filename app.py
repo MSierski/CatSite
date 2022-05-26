6f3bda923bc1a88af822cfdb7321b5854b2ffdcf
sephooko <yellowindmills@gmail.com>
2022-05-26 12:53:12 +0200

    Update - database

--- app.py
+++ app.py
@@ -2,12 +2,47 @@ from flask import Flask
 from flask import request
 from flask import render_template
 from flask import abort, redirect, url_for, make_response
+import sqlite3
 
 app = Flask(__name__, template_folder='./templates', static_folder='./static')
+app.config['SECRET_KEY'] = '2137'
+
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        elif not content:
+            flash('Content is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+                         (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('create.html')
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+@app.route('/database')
+def database():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('database.html', posts=posts)
 
 @app.route('/about')
 def about():
